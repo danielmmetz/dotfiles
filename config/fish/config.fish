534e908e98070b570459547e5689a2e5c96421c6
Daniel Metz <dmetz@lyft.com>
2018-02-09 13:51:45 -0800

    add /usr/local/sbin to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x GOPATH $HOME/go
 set -x DOTFILES $GOPATH/src/github.com/danielmmetz/dotfiles
 set -x WORKSPACE $GOPATH/src/github.com/lyft
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $GOPATH/bin /usr/local/sbin
 set -x PIPENV_SHELL_FANCY 1
 
 alias vi nvim
