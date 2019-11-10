6e9173e350f5310d95b39e4836a2808cac27909e
Daniel Metz <danielmmetz@gmail.com>
2019-11-10 14:37:31 -0800

    support exfat

--- states/nixos/configuration.nix
+++ states/nixos/configuration.nix
@@ -13,6 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
 
   networking.hostName = "metz-x1"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
